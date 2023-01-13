e14c7e6d2f6d968610bf8aec07382520a45a6147
Anish-Rai <arvik.rai3377@gmail.com>
2023-01-13 19:36:19 +0545

    footer component added

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,78 @@
 import React from 'react'
-
+import Link from 'next/link'
+import {BsFacebook,BsInstagram,BsTwitter} from 'react-icons/bs'
+import {FaRegCopyright} from 'react-icons/fa'
 const Footer = () => {
+  const navItems = [
+    {href:'/', name:'Home'},
+    {href:'../#about', name:'About'},
+    {href:'../#services', name:'Services'},
+    {href:'../#portfolio', name:'Portfolio'},
+    {href:'/#Contact', name:'Contact'},
+  ]
+
+  const serviceList = [
+    {serviceName:'Simple/Static Website'},
+    {serviceName:'E-commerce website'},
+    {serviceName:'Simple/Static Website'},
+    {serviceName:'SEO Optimization'},
+    {serviceName:'Web Hosting'},
+    {serviceName:'Digital Marketing'},
+    {serviceName:'Simple/Static Website'},
+  ]
   return (
-    <div>
-      
+    <div className='lg:pt-20   bg-[#26254F] text-white'>
+      <div className='grid grid-cols-3 pb-10 lg:px-44 px-4'>
+        <ul>
+          <h3 className='text-3xl font-semibold pb-3'>Our Services</h3>
+          {
+            serviceList.map(({serviceName},key)=>{
+              return (
+                <li className='leading-[1.9rem]' key={key}> {serviceName} </li>
+              )
+            })
+          }
+        </ul>
+
+        <ul>
+          <h3 className='text-3xl font-semibold pb-3'>Company</h3>
+            {
+              navItems.map(({href,name},key)=>{
+                return (
+                  <li className='leading-[1.9rem]' key={key}>
+                    <Link href={href}> {name} </Link>
+                  </li>
+                )
+              })
+            }
+        </ul>
+
+        <div>
+          <h3 className='text-3xl font-semibold pb-3'>Social Media Links</h3>
+          <ul className='flex text-4xl'>
+            <li className='hover:text-orange-500'> 
+              <Link href="#">  <BsFacebook />  </Link> 
+            </li>
+            <li className='px-10 hover:text-orange-500'> 
+              <Link href="#"> <BsInstagram /> </Link>
+            </li>
+            <li className='hover:text-orange-500'> 
+              <Link href="/"> <BsTwitter /> </Link> 
+            </li>
+          </ul>
+          <ul className='pt-10'>
+            <h3 className='text-2xl  pb-3'>Subscribe for Newsletter</h3>
+            <li> 
+              <input type="email" name="email" id="email" placeholder='Enter your email' className='p-2 pr-7' /> 
+              <button type="submit" className='bg-orange-500 p-2 text-white hover:bg-orange-600'>Submit</button>
+            </li>
+        </ul>
+        </div>
+        
+      </div>
+      <div className=' py-3 bg-black lg:px-44 px-4'>
+        <h4 className='flex items-center'>Copyright 2023 <FaRegCopyright className='mx-2'/> Nepbee Technology Pvt Ltd. </h4>
+      </div>
     </div>
   )
 }
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -22,16 +22,16 @@ const Portfolio = () => {
       <h1 className=' text-2xl lg:text-5xl lg:font-bold font-semibold  lg:pt-0 lg:pb-2 pb-3'>PORTFOLIO</h1>
       <p className='text-lg '>Our recent projects</p>
       <ul className='flex justify-center pt-3'>
-        {projectTypeList.map(({projectType})=>{
+        {projectTypeList.map(({projectType},key)=>{
           return (
-            <li className='px-4 border rounded-full mx-1 mb-2 hover:border-pinkish'>{projectType}</li>
+            <li key={key} className='px-4 border rounded-full mx-1 mb-2 hover:border-pinkish'>{projectType}</li>
           )
         })}
       </ul>
       <div className='grid lg:grid-cols-3 md:grid-cols-2 pt-7 gap-4'>
-        { projectList.map(({projectName})=>{
+        { projectList.map(({projectName},key)=>{
           return (
-            <div>
+            <div key={key}>
               <Image src={`/${projectName}`} alt="" width={400} height={400} className=" w-[100%] h-[100%] transition-transform duration-300 ease-in-out hover:scale-105 py-1" />
             </div>
             
